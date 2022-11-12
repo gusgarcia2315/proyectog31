a9a0dc6c385e92d097f3d15a98c2222a79cec554
gusgarcia2315 <gusgarcia2312@gmail.com>
2022-11-12 12:26:31 -0500

    Commit N 5

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import express from "express";
-import cors from 'cors'
+import cors from 'cors';
+import { router } from './routes/routes.js'
 
 //import blogRoutes from './routes/routes.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-//app.use('/usuarios',blogRoustes)
+//app.use('/usuarios',blogRoutes)
+app.use('/',router);
 
 
-app.get('/',(req,res)=>{
-    res.send('-_- hola mundo :V')
-})
 
 app.listen(8000, ()=> {
-    console.log('servidor Corriente en http://localhost:8000/')
+    console.log('servidor Corriendo en  http://localhost:8000/')
 })
